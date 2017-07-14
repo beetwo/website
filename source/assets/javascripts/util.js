3c212a2af002800f56fb6ba62140fc8a17ee9949
initial commit <git@lowi.org>
2017-07-14 10:51:24 +0200

    as minimal as it gets

--- source/assets/javascripts/util.js
+++
@@ -1,7 +0,0 @@
-export default {
-  imagePath: '/leppers/assets/images',
-
-  // detects mobile browsers based on screen width
-  // (analogous to/in accordance with to css breakpoints)
-  isMobile: () => { return $(window).width() < 768 }
-}
